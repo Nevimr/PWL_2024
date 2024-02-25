a3075c2e6a84e3433bc29979f7f3c2f5db6df040
Nevi Marita Rimadhani <2131710025@student.polinema.ac.id>
2024-02-25 14:38:03 +0700

    Job2_MembuatController

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,11 @@
 <?php
 
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PageController;
+use App\Http\Controllers\PhotoController;
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,20 +22,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/hello', function () {
-    return 'Hello World';
-});
+
+Route::get('/hello', [WelcomeController::class,'hello']);
+
 Route::get('/world', function () {
     return 'World';
 });
-Route::get('/', function () {
-    return 'Selamat Datang';
-});
-Route::get('/about', function () {
-    NIM     : '2141762025';
-    Nama    : 'Nevi Marita Rimadhani';
-    return 'NIM     : 2141762025, Nama  : Nevi Marita Rimadhani';
-});
+
+Route::get('/index', [HomeController::class,'index']);
+
+Route::get('/about', [AboutController::class,'about']);
+
 Route::get('/user/{name}', function ($name) {
     return 'Nama saya '.$name;
 });
@@ -37,10 +40,11 @@ Route::get('/posts/{post}/comments/{comment}', function
 ($postId, $commentId) {
     return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
 });
-Route::get('/articles/{id}', function ($id) {
-    return 'Halaman Artikel dengan ID '.$id;
-});
+
+Route::get('/articles/{id}', [ArticleController::class,'articles']);
 
 Route::get('/user/{name?}', function ($name='John') {
     return 'Nama saya '.$name;
 });
+
+Route::resource('photos', PhotoController::class);
